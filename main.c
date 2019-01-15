9c114db7c912c6d341dca81f04713e479e68a659
yy <1019624863@qq.com>
2019-01-15 10:56:50 +0800

    基本模块与存钱

--- main.c
+++ main.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int qian=0；
+    printf("进入钱包管理系统");
+    {
+        while(1)
+        {
+            printf("1---存钱\n");
+            printf("2---取钱\n");
+            printf("3---余额\n");
+            printf("4---退出\n");
 
-    printf("hello world!");
+            printf("请选择相应功能\n");
+            int code;
+            scanf("%d",&code);
+
+            if(code==1)
+            {
+                printf("");
+                int cun;
+                scanf("%d",cun);
+                qian += cun;
+
+                printf("存入成功，回车继续\n");
+                char x;
+                scanf("%c",&x):
+                scanf("%c",&x):
+            }
+            if(code=2)
+            {
+                
+            }
+            if(code=3)
+            {
+                
+            }
+            if(code=4)
+            {
+                
+            }
+        }
+    }
+    
 
     return 0;
 
